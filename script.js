78df86aff379023ada897cd20d5605794c596d2e
mdahmed4875 <140884439+mdahmed4875@users.noreply.github.com>
2025-08-14 09:13:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function threeSum(arr, target) {
   //your code here
+	let n=arr.length;
+	arr.sort((a,b)=>a-b);
+	let ans=arr[0]+arr[1]+arr[2];
+	for(let i=0;i<n-2;i++){
+		let l=i+1;
+		let r=n-1;
+		while(l<r){
+			let sum=arr[i]+arr[l]+arr[r];
+			if(Math.abs(sum-target)<Math.abs(ans-target)){
+				ans=sum;
+			}
+			if(sum<target){
+				l++;
+			}
+			else r++;
+		}
+	}
+	return ans;
+	
 }
 
 module.exports = threeSum;
